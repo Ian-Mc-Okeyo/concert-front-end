efbc3447b7c5c06fd1e955d1e4777737d754f853
Newton Black <newtieblackie@gmail.com>
2023-01-18 19:41:04 +0300

    React icons installation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => {
-    return(
+export default function Home() {
+  return (
     <>
-        <div className="bg"></div>
-        <div className="bg bg2"></div>
-        <div className="bg bg3"></div>
-        <div className="content">
-            <h1>BRIGHTEN YOUR CORNER CONCERT</h1>
-            <Link to ='/buy' className='btn btn-primary'>Get Your ticket</Link>
-        </div>
-    </>
-    )
-}
+      <div className="bg"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
+      <div className="content">
+        <h1>BRIGHTEN YOUR CORNER CONCERT</h1>
+        <Link to="/buy">
+          <button className="" type="submit">
+            RSVP
+          </button>
+        </Link>
+        {/* ...........footer section............ */}
 
-export default Home
+        {/* .....footer section ends........ */}
+      </div>
+    </>
+  );
+}
