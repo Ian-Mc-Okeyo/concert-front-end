0a00a0d4537232cc6aadb482be68fc71d1e5bb71
IAN MARK <94452429+Ian-Mc-Okeyo@users.noreply.github.com>
2023-03-03 08:48:06 +0300

    Update index.js

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -13,10 +13,11 @@ export const basicSchema = yup.object().shape({
     phone_number: yup.string().required("Required").min(10, "Invalid Phone Number").max(10, 'Invalid Phone number').
         test("Check prefix", function () {
             let code = "07";
+            let code_2 = "01";
             let num = this.parent["phone_number"];
             // console.log(carriercode, blnum);
             if (code && num) {
-                if(num.startsWith(code) && containsOnlyNumbers(num)){
+                if((num.startsWith(code) || num.startsWith(code_2) ) && containsOnlyNumbers(num)){
                     return true;
                 }
                 else{
